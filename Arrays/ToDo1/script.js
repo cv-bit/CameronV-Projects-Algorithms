7275c955e1865d2712bcd38e82e4d9d2cbbe7454
cv-bit <ververcameron29@gmail.com>
2021-01-14 14:31:32 -0600

    arrays todo1

---
+++ Arrays/ToDo1/script.js
@@ -0,0 +1,54 @@
+
+//push front
+function pushFront(arr, value) {
+	for(let i = arr.length; i > 0; i--)
+		arr[i] = arr[i-1]
+	
+	arr[0] = value;
+}
+
+//pop front
+function popFront(arr) {
+	const val = arr[0];
+	for(let i = 0; i < arr.length; i++)
+		arr[i] = arr[i + 1];
+	arr.length = arr.length - 1;
+	return val;
+}
+
+//insert at
+function insertAt(arr, idx, val) {
+	for(let i = arr.length; i > idx; i--)
+		arr[i] = arr[i-1]
+	
+	arr[idx] = val;
+}
+
+//remove at
+function removeAt(arr, idx) {
+	toRemove = arr[idx];
+	for(let i = idx; i < arr.length-1; i++) {
+        arr[i] = arr[i+1];
+    }
+    arr.length = arr.length-1;
+    return toRemove;
+}
+
+//swap pairs
+function swapPairs(arr) {
+	for(let i = 0; i < arr.length - 1; i = i + 2) {
+		let temp = arr[i];
+		arr[i] = arr[i + 1];
+		arr[i + 1] = temp;
+	}
+}
+
+//remove duplicates
+function removeDupesUnnested(arr) {
+	let newArr = [];
+	for(let i = 0; i < arr.length; i++) {
+		if(arr[i] !== arr[i+1]) 
+			newArr.push(arr[i])
+	}
+	return newArr;
+}
